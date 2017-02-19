6d9f5492c03e41eb289d677ac0d9b9823369614e
daksh anand <dakshanand@gmail.com>
2017-02-19 20:36:50 +0530

    MTDF with Bugs

--- P3.py
+++ P3.py
@@ -9,11 +9,17 @@ class player3():
 	def __init__(self):
 		self.block = [[0 for x in range(4)] for y in range(4)]
 		self.hash_block = [[0 for x in range(4)] for y in range(4)]
-		self.zobrist = []
+		self.hash_board = 0
+		self.transposition_table = {}
+		self.heuristic_table = {}
+		self.zobrist_board = []
+		for x in range(512):
+			self.zobrist_board.append(random.randint(0,1<<63))
+		self.zobrist_block = []
 		value = 1
 		for x in range(32):
-			self.zobrist.append(value)
-			value =* 2
+			self.zobrist_block.append(value)
+			value *= 2
 
 	def heuristics_player(self):
 		score = 0
@@ -74,6 +80,7 @@ class player3():
 
 	def heuristics_opponent(self):
 		score = 0
+
 		for i in range(4):
 			for j in range(4):
 				if self.local_board.block_status[i][j] == self.opponent:
@@ -312,60 +319,6 @@ class player3():
 
 		return score
 
-	def alphabeta (self, depth, alpha, beta, maximini, old_move):
-		terminal_status = self.local_board.find_terminal_state()
-		if depth == 0 or terminal_status[0] != "CONTINUE":
-			if terminal_status[0] == self.player:
-				return 160
-			elif terminal_status[0] != self.player and terminal_status[1] == "WON":
-				return -160
-			elif terminal_status[1] == "DRAW":
-				score = 0
-				for i in range(4):
-					for j in range(4):
-						if self.block_status[i][j] == self.player:
-							score += 7
-						if self.block_status[i][j] == self.opponent:
-							score -= 7
-				return score
-
-			return (self.heuristics_player() + self.heuristics_opponent() + self.heuristics_player_block() - self.heuristics_opponent_block())
-
-		if maximini:
-			v = -1000
-			valid_moves = self.local_board.find_valid_move_cells (old_move)
-			for valid_move in valid_moves:
-				self.local_board.update(old_move,valid_move,self.player)
-				self.last_move = valid_move
-				new_v = self.alphabeta (depth - 1, alpha, beta, 0, valid_move)
-				self.local_board.board_status[valid_move[0]][valid_move[1]]='-'
-				x = valid_move[0]/4
-				y = valid_move[1]/4
-				self.local_board.block_status[x][y] = '-'
-				if new_v > v:
-					if self.level==depth:
-						self.best_move = valid_move
-					v = new_v
-				alpha = max(alpha, v)
-				if beta <= alpha:
-					break
-			return v
-		else:
-			v = 1000
-			valid_moves = self.local_board.find_valid_move_cells (old_move)
-			for valid_move in valid_moves:
-				self.local_board.update(old_move, valid_move, self.opponent)
-				self.last_move = valid_move
-				v = min(v, self.alphabeta (depth - 1, alpha, beta, 1, valid_move))
-				self.local_board.board_status[valid_move[0]][valid_move[1]]='-'
-				x = valid_move[0]/4
-				y = valid_move[1]/4
-				self.local_board.block_status[x][y] = '-'
-				beta = min(beta, v)
-				if beta <= alpha:
-					break
-			return v
-
 	def signal_handler(self, signum, frame):
     		raise Exception("Timed out!")
 
@@ -376,33 +329,127 @@ class player3():
 				result = 0
 				for i in range(4):
 					for j in range(4):
-						current = self.local_board[4*p+i][4*p+j]
+						current = self.local_board.board_status[4*p+i][4*q+j]
 						if current == self.player:
-							result = result ^ self.zobrist[x*2+1]
+							result = result ^ self.zobrist_block[x*2+1]
 						elif current == self.opponent:
-							result = result ^ self.zobrist[x*2+0]
+							result = result ^ self.zobrist_block[x*2+0]
 						x += 1
 				self.hash_block[p][q] = result
+
+		x = 0
+		result = 0
+		for p in range(16):
+			for q in range(16):
+				current = self.local_board.board_status[p][q]
+				if current == self.player:
+					result = result ^ self.zobrist_board[x*2+1]
+				elif current == self.opponent:
+					result = result ^ self.zobrist_board[x*2+0]
+				x += 1
+		self.hash_board = result
+
+	# 0 = minScore
+	# 1 = maxScore
+	# 2 = bestMove
+	# 3 = depth
+	def test(self, maxDepth, currentDepth, gamma, old_move, flag):
+
+		if self.hash_board in self.transposition_table:
+			entry = self.transposition_table[self.hash_board]
+			if entry[3] > maxDepth - currentDepth:
+				if entry[0] > gamma:
+					return entry[0], entry[2]
+				if entry[1] < gamma:
+					return entry[1], entry[2]
+		else:
+			self.transposition_table[self.hash_board] = [-1000,1000,(-1,-1),maxDepth - currentDepth]
+
+		# check if we're done recursing, gameover condition
+
+		terminal_status = self.local_board.find_terminal_state()
+		if currentDepth == maxDepth or terminal_status[0] != "CONTINUE":
+			if terminal_status[0] == self.player:
+				self.transposition_table[self.hash_board][0] = self.transposition_table[self.hash_board][1] = 160
+			elif terminal_status[0] != self.player and terminal_status[1] == "WON":
+				self.transposition_table[self.hash_board][0] = self.transposition_table[self.hash_board][1] = -160
+			elif terminal_status[1] == "DRAW":
+				score = 0
+				for i in range(4):
+					for j in range(4):
+						if self.block_status[i][j] == self.player:
+							score += 7
+						if self.block_status[i][j] == self.opponent:
+							score -= 7
+				self.transposition_table[self.hash_board][0] = self.transposition_table[self.hash_board][1] = score
+			else:
+				self.transposition_table[self.hash_board][0] = self.transposition_table[self.hash_board][1] = self.heuristics_player() + self.heuristics_opponent() + self.heuristics_player_block() - self.heuristics_opponent_block()
+
+			return self.transposition_table[self.hash_board][0], self.transposition_table[self.hash_board][2]
+
+		bestMove = (-1,-1)
+		bestScore = -160
+
+		valid_moves = self.local_board.find_valid_move_cells(old_move)
+		for valid_move in valid_moves:
+			if flag == 1:
+				self.local_board.update(old_move,valid_move,self.player)
+			else:
+				self.local_board.update(old_move,valid_move,self.opponent)
+			self.last_move = valid_move
+			self.hash_board ^= self.zobrist_board[2*(valid_move[0]*16+valid_move[1])+flag]
+			self.hash_block[valid_move[0]/4][valid_move[1]/4] ^= self.zobrist_block[2*((valid_move[0]%4)*4+valid_move[1]%4)+flag]
+			#recurse
+			recursedScore, currentMove = self.test(maxDepth, currentDepth+1, -gamma, valid_move, (flag+1)%2)
+
+			currentScore = -recursedScore
+			self.hash_board ^= self.zobrist_board[2*(valid_move[0]*16+valid_move[1])+flag]
+			self.hash_block[valid_move[0]/4][valid_move[1]/4] ^= self.zobrist_block[2*((valid_move[0]%4)*4+valid_move[1]%4)+flag]
+			self.local_board.board_status[valid_move[0]][valid_move[1]]='-'
+			x = valid_move[0]/4
+			y = valid_move[1]/4
+			self.local_board.block_status[x][y] = '-'
+			if currentScore > bestScore:
+				self.transposition_table[self.hash_board][2] = bestMove = valid_move
+				bestScore = currentScore
+			# If we pruned, then we have a min score, otherwise we have a max score.
+			if bestScore < gamma:
+				self.transposition_table[self.hash_board][1] = bestScore
+			else:
+				self.transposition_table[self.hash_board][0] = bestScore
+
+		return bestScore, bestMove
+
+	def mtd(self, maxDepth, guess, old_move):
+		for i in range(1):
+			gamma = guess
+			guess, move = self.test(maxDepth, 0, gamma-1, old_move, 1)
+			#if there is no improvement, stop looking
+			if gamma == guess:
+				break
+		return guess, move
+
+	def mtdf(self, maxDepth, old_move):
+		guess = 0
+		# Iteratively deepen the search
+		signal.signal(signal.SIGALRM, self.signal_handler)
+		signal.alarm(15)
+		try:
+			for i in range(2,maxDepth):
+
+				guess, move = self.mtd(i,guess, old_move)
+				print i
+				self.local_board = self.saved
+		except Exception, msg:
+			return move
 
 	def move(self, board, old_move, flag):
 		self.player = flag
 		self.opponent = "x" if self.player == "o" else "o"
-		saved=copy.deepcopy(board)
+		self.saved = copy.deepcopy(board)
 		self.local_board = board
-		signal.signal(signal.SIGALRM, self.signal_handler)
-		signal.alarm(100)
-		g=0
+		self.hash()
+		self.last_move = (0,0)
 
-		try:
-			for i in range(1,100):
-				self.level=i
-				g = self.mtdf(g,i,old_move)
-				best = self.best_move
-				self.local_board=saved
-				print i
-
-
-		except Exception, msg:
-			pass
-
-		return best
+		bestMove = self.mtdf(20, old_move)
+		return bestMove
