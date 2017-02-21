de5130f14806a4460e34b9cde7fe28f3e45e59f5
daksh anand <dakshanand@gmail.com>
2017-02-21 12:58:45 +0530

    Heuristic Issues solved

--- P2.py
+++ P2.py
@@ -27,18 +27,18 @@ class player2():
 			if self.local_board.block_status[i].count(self.opponent) == 0:
 				player_count = self.local_board.block_status[i].count(self.player)
 				if player_count == 2:
-					score += 5
-				elif player_count == 3:
 					score += 15
+				elif player_count == 3:
+					score += 35
 
 			# for 2 and 3 blocks in a column
 			col = [x[i] for x in self.local_board.block_status]			#i'th column
 			if col.count(self.opponent) == 0:
 				player_count = col.count(self.player)
 				if player_count == 2:
-					score += 5
-				elif player_count == 3:
 					score += 15
+				elif player_count == 3:
+					score += 35
 
 		# for 2 and 3 blocks in the \ diagonal
 		diagonal_1 = []
@@ -48,9 +48,9 @@ class player2():
 		if diagonal_1.count(self.opponent) == 0:
 			player_count = diagonal_1.count(self.player)
 			if player_count == 2:
-				score += 5
-			elif player_count == 3:
 				score += 15
+			elif player_count == 3:
+				score += 35
 
 		# for 2 and 3 blocks in the ? diagonal
 		diagonal_2 = []
@@ -60,9 +60,9 @@ class player2():
 		if diagonal_2.count(self.opponent) == 0:
 			player_count = diagonal_2.count(self.player)
 			if player_count == 2:
-				score += 5
-			elif player_count == 3:
 				score += 15
+			elif player_count == 3:
+				score += 35
 
 		return score
 
@@ -71,28 +71,28 @@ class player2():
 		for i in range(4):
 			for j in range(4):
 				if self.local_board.block_status[i][j] == self.opponent:
-					score -= 10
+					score += 10
 					if i >= 1 and i <= 2 and j >= 1 and j <= 2:
-						score -= 10
+						score += 10
 					elif ((i == 0 and (j == 0 or j == 3 )) or (i == 3 and (j == 0 or j == 3 ))):
-						score -= 4
+						score += 4
 
 			# for 2 and 3 blocks in a row
 			if self.local_board.block_status[i].count(self.player) == 0:
 				opponent_count = self.local_board.block_status[i].count(self.opponent)
 				if opponent_count == 2:
-					score += 5
-				elif opponent_count == 3:
 					score += 15
+				elif opponent_count == 3:
+					score += 35
 
 			# for 2 and 3 blocks in a column
 			col = [x[i] for x in self.local_board.block_status]			#i'th column
 			if col.count(self.player) == 0:
 				opponent_count = col.count(self.opponent)
 				if opponent_count == 2:
-					score += 5
-				elif opponent_count == 3:
 					score += 15
+				elif opponent_count == 3:
+					score += 35
 
 		# for 2 and 3 blocks in the \ diagonal
 		diagonal_1 = []
@@ -102,9 +102,9 @@ class player2():
 		if diagonal_1.count(self.player) == 0:
 			opponent_count = diagonal_1.count(self.opponent)
 			if opponent_count == 2:
-				score += 5
-			elif opponent_count == 3:
 				score += 15
+			elif opponent_count == 3:
+				score += 35
 
 		# for 2 and 3 blocks in the ? diagonal
 		diagonal_2 = []
@@ -114,9 +114,9 @@ class player2():
 		if diagonal_2.count(self.player) == 0:
 			opponent_count = diagonal_2.count(self.opponent)
 			if opponent_count == 2:
-				score += 5
-			elif opponent_count == 3:
 				score += 15
+			elif opponent_count == 3:
+				score += 35
 
 		return score
 
@@ -161,11 +161,11 @@ class player2():
 					if self.block[i].count(self.opponent) == 0:
 						player_count = self.block[i].count(self.player)
 						if player_count == 2:
-							score += 0.5
+							score += 1.5
 							if center:
 								score += 0.2
 						elif player_count == 3:
-							score += 1.5
+							score += 3.5
 							if center:
 								score += 0.2
 
@@ -174,11 +174,11 @@ class player2():
 					if col.count(self.opponent) == 0:
 						player_count = col.count(self.player)
 						if player_count == 2:
-							score += 0.5
+							score += 1.5
 							if center:
 								score += 0.2
 						elif player_count == 3:
-							score += 1.5
+							score += 3.5
 							if center:
 								score += 0.2
 
@@ -190,11 +190,11 @@ class player2():
 				if diagonal_1.count(self.opponent) == 0:
 					player_count = diagonal_1.count(self.player)
 					if player_count == 2:
-						score += 0.5
+						score += 1.5
 						if center:
 							score += 0.2
 					elif player_count == 3:
-						score += 1.5
+						score += 3.5
 						if center:
 							score += 0.2
 
@@ -206,11 +206,11 @@ class player2():
 				if diagonal_2.count(self.opponent) == 0:
 					player_count = diagonal_2.count(self.player)
 					if player_count == 2:
-						score += 0.5
+						score += 1.5
 						if center:
 							score += 0.2
 					elif player_count == 3:
-						score += 1.5
+						score += 3.5
 						if center:
 							score += 0.2
 
@@ -251,11 +251,11 @@ class player2():
 					if self.block[i].count(self.player) == 0:
 						opponent_count = self.block[i].count(self.opponent)
 						if opponent_count == 2:
-							score += 0.5
+							score += 1.5
 							if center:
 								score += 0.2
 						elif opponent_count == 3:
-							score += 1.5
+							score += 3.5
 							if center:
 								score += 0.2
 
@@ -264,11 +264,11 @@ class player2():
 					if col.count(self.player) == 0:
 						opponent_count = col.count(self.opponent)
 						if opponent_count == 2:
-							score += 0.5
+							score += 1.5
 							if center:
 								score += 0.2
 						elif opponent_count == 3:
-							score += 1.5
+							score += 3.5
 							if center:
 								score += 0.2
 
@@ -280,11 +280,11 @@ class player2():
 				if diagonal_1.count(self.player) == 0:
 					opponent_count = diagonal_1.count(self.opponent)
 					if opponent_count == 2:
-						score += 0.5
+						score += 1.5
 						if center:
 							score += 0.2
 					elif opponent_count == 3:
-						score += 1.5
+						score += 3.5
 						if center:
 							score += 0.2
 
@@ -296,11 +296,11 @@ class player2():
 				if diagonal_2.count(self.player) == 0:
 					opponent_count = diagonal_2.count(self.opponent)
 					if opponent_count == 2:
-						score += 0.5
+						score += 1.5
 						if center:
 							score += 0.2
 					elif opponent_count == 3:
-						score += 1.5
+						score += 3.5
 						if center:
 							score += 0.2
 
@@ -323,7 +323,7 @@ class player2():
 							score -= 7
 				return score
 
-			return (self.heuristics_player() + self.heuristics_opponent() + self.heuristics_player_block() - self.heuristics_opponent_block())
+			return (self.heuristics_player() - self.heuristics_opponent() + self.heuristics_player_block() - self.heuristics_opponent_block())
 
 		if maximini:
 			v = -1000
@@ -374,11 +374,14 @@ class player2():
 		signal.alarm(15)
 
 		try:
-			for i in range(4,100):
-				self.level=i
+			for i in range(3,5):
+				self.level = i
 				self.alphabeta (i, -1000, 1000, 1, old_move)
+				print i
 				best = self.best_move
-				self.local_board=saved
+				self.local_board = saved
+
+
 
 		except Exception, msg:
 			pass
